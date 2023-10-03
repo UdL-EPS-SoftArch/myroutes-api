7e35d6e24fe405f9a41c0d077dca7b553b4e9953
Gerard <gerardmonso@gmail.com>
2023-10-03 17:47:05 +0200

    coordinate relationships

--- src/main/java/cat/udl/eps/softarch/myroutes/domain/Coordinate.java
+++ src/main/java/cat/udl/eps/softarch/myroutes/domain/Coordinate.java
@@ -1,9 +1,11 @@
 package cat.udl.eps.softarch.myroutes.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -21,5 +23,9 @@ public class Coordinate extends UriEntity<Long> {
     @NotBlank
     @NotNull
     private String coordinate;
+
+    //@ManyToOne
+    //@JsonIdentityReference(alwaysAsId = true)
+    //private RouteVersion offerer;
 
 }
