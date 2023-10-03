f0745fa25fa3c00557d04afd3c6444af8c6ee951
Gerard <gerardmonso@gmail.com>
2023-10-03 15:32:29 +0200

    Fix Coordinate entity

--- src/main/java/cat/udl/eps/softarch/myroutes/domain/Coordinate.java
+++ src/main/java/cat/udl/eps/softarch/myroutes/domain/Coordinate.java
@@ -1,27 +1,25 @@
 package cat.udl.eps.softarch.myroutes.domain;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
-@EqualsAndHashCode(callSuper = true)
-public class Coordinate extends UriEntity<String> {
+public class Coordinate extends UriEntity<Long> {
 
     @Id
     @GeneratedValue()
     private Long id;
 
     @NotBlank
+    @NotNull
     private String coordinate;
-
-    @Override
-    public String getId() {
-        return null;
-    }
 
 }
