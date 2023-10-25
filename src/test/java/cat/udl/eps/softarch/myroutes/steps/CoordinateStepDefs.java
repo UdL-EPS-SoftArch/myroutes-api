44a588f4f3b05d04160cb2c47e424b5392455fbe
Gerard <gerardmonso@gmail.com>
2023-10-25 19:02:05 +0200

    retrieve coordinate cucumber scenario

--- src/test/java/cat/udl/eps/softarch/myroutes/steps/CoordinateStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/myroutes/steps/CoordinateStepDefs.java
@@ -2,6 +2,7 @@ package cat.udl.eps.softarch.myroutes.steps;
 
 import cat.udl.eps.softarch.myroutes.domain.Coordinate;
 import cat.udl.eps.softarch.myroutes.repository.CoordinateRepository;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +11,8 @@ import java.nio.charset.StandardCharsets;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 
 public class CoordinateStepDefs {
 
@@ -18,6 +21,7 @@ public class CoordinateStepDefs {
 
     @Autowired
     private CoordinateRepository coordinateRepository;
+    private Coordinate currentCoordinate;
 
     @When("I create a new Coordinate with value {string}")
     public void iCreateANewCoordinateWithValue(String s_coordinate) throws Throwable {
@@ -35,4 +39,27 @@ public class CoordinateStepDefs {
                 .andDo(print());
     }
 
+    @And("I create a new valid Coordinate")
+    public void iCreateANewValidCoordinate() throws Throwable{
+        Coordinate coordinate = new Coordinate();
+        coordinate.setCoordinate("41.40338,2.17403");
+        coordinateRepository.save(coordinate);
+        this.currentCoordinate = coordinate;
+    }
+
+    @When("I get that Coordinate")
+    public void iGetThatCoordinate() throws Throwable{
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/coordinates/{id}", this.currentCoordinate.getId())
+                        .with(AuthenticationStepDefs.authenticate())
+                        .accept(MediaType.APPLICATION_JSON));
+    }
+
+    @When("I try to retrieve a Coordinate with id {long}")
+    public void iTryToRetrieveACoordinateWithId(Long id) throws Throwable{
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/coordinates/{id}", id)
+                        .with(AuthenticationStepDefs.authenticate())
+                        .accept(MediaType.APPLICATION_JSON));
+    }
 }
---
+++ src/test/resources/features/RetrieveCoordinate.feature
@@ -0,0 +1,18 @@
+Feature: Retrieve Coordinate
+  In order to use the app
+  As a user
+  I want to retrive coordinate and check throw errors
+
+  Background:
+    Given There is a registered user with username "username" and password "password" and email "user@domain.com"
+
+  Scenario: Retrieve coordinate
+    Given I login as "username" with password "password"
+    And I create a new valid Coordinate
+    When I get that Coordinate
+    Then The response code is 200
+
+  Scenario: Get a Coordinate which does not exist
+    Given I login as "username" with password "password"
+    When I try to retrieve a Coordinate with id 23535
+    Then The response code is 404
