c64434cbd1771da850aa73bf40c09882a185d11a
Dani <dbistuer@msn.com>
2023-11-23 09:25:40 +0100

    creating containing for the front search list

--- src/main/java/cat/udl/eps/softarch/myroutes/repository/RouteRepository.java
+++ src/main/java/cat/udl/eps/softarch/myroutes/repository/RouteRepository.java
@@ -12,4 +12,5 @@ import java.util.List;
 public interface RouteRepository extends CrudRepository<Route, Long>, PagingAndSortingRepository<Route, Long> {
     List<Route> findByCreatedBy(@Param("creator") User creator);
     List<Route> findByTitle(@Param("title") String title);
+    List<Route> findByTitleContainingIgnoreCase(@Param("title") String title);
 }
