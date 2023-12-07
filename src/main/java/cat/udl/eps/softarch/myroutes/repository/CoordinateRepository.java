71360196d6935a71287d56c562008239b043e464
Gerard <gerardmonso@gmail.com>
2023-12-07 02:46:09 +0100

    coordinates entity improve and custom functions to search coordinates

--- src/main/java/cat/udl/eps/softarch/myroutes/repository/CoordinateRepository.java
+++ src/main/java/cat/udl/eps/softarch/myroutes/repository/CoordinateRepository.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.softarch.myroutes.repository;
 
 import cat.udl.eps.softarch.myroutes.domain.Coordinate;
+import cat.udl.eps.softarch.myroutes.domain.RouteVersion;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,5 +11,7 @@ import java.util.List;
 
 @RepositoryRestResource
 public interface CoordinateRepository extends CrudRepository<Coordinate, Long>, PagingAndSortingRepository<Coordinate, Long> {
-    // public boolean findCoordinateByRouteVersion(@Param("routeVersion") RouteVersion routeVersion);
+    List<Coordinate> findByRouteVersion(@Param("routeVersion") RouteVersion routeVersion);
+    List<Coordinate> findByCoordinate(@Param("coordinate") String coordinate);
+
 }
